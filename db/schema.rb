af51be22eaf0107df1d746f07c9098fcd382d74c
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-03-29 22:20:45 +0900

    テーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_28_125300) do
+ActiveRecord::Schema.define(version: 2023_03_29_130527) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2023_03_28_125300) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.datetime "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
