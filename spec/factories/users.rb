d9d7bd0b4098410afd34fe39f2e8d3e4215270e3
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-03-29 19:29:20 +0900

    ユーザー管理機能、単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'a00000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     first_name            {'山田'}
     last_name             {'太郎'}
     first_name_kana       {'ヤマダ'}
     last_name_kana        {'タロウ'}
-    birthday              {'1930/1/1'}
+    birthday              {'1930-01-01'}
   end
 end
