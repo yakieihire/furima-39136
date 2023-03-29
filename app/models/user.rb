dacb624b6ba292ab607a11218adfaf7a5826be7a
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-03-29 20:35:32 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,           presence: true
-  validates :email,              presence: true
-  validates :password,           presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
   validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'カナ全角を使用してください' }
-  validates :last_name_kana,    presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'カナ全角を使用してください' }
+  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナ全角を使用してください' }
+  validates :last_name_kana,    presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナ全角を使用してください' }
   validates :birthday,           presence: true
 end
