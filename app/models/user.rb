60b17223288b851effd7f0550cce5e665673d7cd
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-03-30 11:20:25 +0900

    userモデル記述変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,           presence: true
+  validates :password,                           format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,message:'には英字と数字の両方を含めて設定してください' }
   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
   validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
   validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナ全角を使用してください' }
-  validates :last_name_kana,    presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナ全角を使用してください' }
+  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナ全角を使用してください' }
   validates :birthday,           presence: true
 end
