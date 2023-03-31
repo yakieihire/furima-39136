48ff1540f075fa69287e0f498ac5a5ce7cde51b8
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-03-31 17:07:58 +0900

    各モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :shipping_payer
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :situation_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipping_payer_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :price, presence:true
 end
