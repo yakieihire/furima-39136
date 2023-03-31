42f13057311c8db1e6d7f9bc57f91535a79b1bdd
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-03-31 22:45:11 +0900

    モデルの設定修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,13 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :situation_id, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :shipping_payer_id, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :category_id, numericality: { other_than: 1} 
+  validates :situation_id, numericality: { other_than: 1} 
+  validates :shipping_payer_id, numericality: { other_than: 1} 
+  validates :prefecture_id, numericality: { other_than: 1} 
+  validates :shipping_day_id, numericality: { other_than: 1} 
   validates :price, presence:true, numericality:{ only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 end
