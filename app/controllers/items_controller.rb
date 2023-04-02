844259f6a31a93a25aa8ce16565a47743700a079
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-02 21:03:51 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if @item.user_id == current_user.id
+      @item.destroy
       redirect_to root_path
     else
       render :show
