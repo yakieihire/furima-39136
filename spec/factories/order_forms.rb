ba2d082a260d4e9c0e4d78da48b6241b8a8e69e5
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-04 22:34:22 +0900

    order_formテストコード修正

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     address                { '1-1' }
     building_name          { '建物' }
     telephone_number       { '00011112222' }
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
