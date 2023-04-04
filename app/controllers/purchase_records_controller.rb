1a7bf6169a22c7f01b7f34570ed12b487a2cb349
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-04 22:32:55 +0900

    pay.jp設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -8,6 +8,7 @@ class PurchaseRecordsController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -17,6 +18,15 @@ class PurchaseRecordsController < ApplicationController
 
   private
   def order_params
-    params.require(:order_form).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_form).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,14 +1,14 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id, numericality: { other_than: 1}
     validates :municipality
     validates :address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/,}
-    
   end
 
 
