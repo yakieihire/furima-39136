e1ea1be4c8b5d646a8d4191c9acbf3d442b04306
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-09 21:42:58 +0900

    deploy.rb修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :application, 'furima-39136'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yakieihire/furima-39136.git'
+set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
